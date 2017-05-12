e0d3ec6ebf57a7b9ed0a12ed83c392b2466ebe88
Joey Qinag <joeyqiang@gmail.com>
2017-05-11 22:49:26 -0500

    UPDATE

--- js/engine.js
+++ js/engine.js
@@ -1,172 +1,104 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
- */
-
-var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas elements height/width and add it to the DOM.
-     */
+// 传入全局变量 this->global
+var Engine = (function (global, width, height) {
+    // 定义绘制动画需要的参数
     var doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
+        // 定义画布为全局变量，便于其他 JS 文件调用
+        global.ctx = ctx;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    // 定义画布大小
+    canvas.width = width;
+    canvas.height = height;
     doc.body.appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
-     */
-    function main() {
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
-         */
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
-
-
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
-        update(dt);
-        render();
-
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
-
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
-        win.requestAnimationFrame(main);
-    }
-
-    /* This function does some initial setup that should only occur once,
-     * particularly setting the lastTime variable that is required for the
-     * game loop.
-     */
+    // 初始化函数
     function init() {
+        // 还原
         reset();
+        // 时间戳，记录的是千分之一秒值
         lastTime = Date.now();
+        // 主函数
         main();
     }
 
-    /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. Based on how
-     * you implement your collision detection (when two entities occupy the
-     * same space, for instance when your character should die), you may find
-     * the need to add an additional function call here. For now, we've left
-     * it commented out - you may or may not want to implement this
-     * functionality this way (you could just implement collision detection
-     * on the entities themselves within your app.js file).
-     */
+
+    // 主函数，画布不断刷新，同时更新画布内容
+    function main() {
+        var now = Date.now();
+        dt = (now - lastTime) / 1000
+        // 画面内容更新，显示器未更新
+        update(dt);
+        // 画面内容渲染，显示器未更新
+        render();
+        // 时间戳
+        lastTime = now;
+        // Javascript 每 1/60 FPS 刷新一次，类似 setTimeout(main, 1000/60)
+        win.requestAnimationFrame(main);
+    }
+
+    // 还原函数
+    function reset() {
+        // 游戏重新执行
+        // restart();
+    }
+
+    // 更新函数
     function update(dt) {
-        updateEntities(dt);
+        updateUntities(dt);
+        // 检验是否发生碰撞
         // checkCollisions();
     }
 
-    /* This is called by the update function and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
-     * their update() methods. It will then call the update function for your
-     * player object. These update methods should focus purely on updating
-     * the data/properties related to the object. Do your drawing in your
-     * render methods.
-     */
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
+    // 更新细节
+    function updateUntities(dt) {
+        // 玩家更新，一个玩家
+        player.update();
+        // 敌人更新，多个敌人
+        allEnemies.forEach(function (enemy) {
             enemy.update(dt);
         });
-        player.update();
     }
 
-    /* This function initially draws the "game level", it will then call
-     * the renderEntities function. Remember, this function is called every
-     * game tick (or loop of the game engine) because that's how games work -
-     * they are flipbooks creating the illusion of animation but in reality
-     * they are just drawing the entire screen over and over.
-     */
+    // 渲染函数
     function render() {
-        /* This array holds the relative URL to the image used
-         * for that particular row of the game level.
-         */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png',   // 第一行图片
+                'images/stone-block.png',   // 第二行图片
+                'images/stone-block.png',   // 第三行图片
+                'images/stone-block.png',   // 第四行图片
+                'images/grass-block.png',   // 第五行图片
+                'images/grass-block.png'    // 第六行图片
             ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
+            rows = 6,
+            cols = 5,
+            row,
+            col;
 
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+        // 定位背景图片位置，图片大小 101 * 83
+        for (row = 0; row < rows; row++) {
+            for (col = 0; col < cols; col++) {
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 82);
             }
         }
 
-        renderEntities();
+        // 渲染操作元素
+        renderUntities();
     }
 
-    /* This function is called by the render function and is called on each game
-     * tick. Its purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
-     */
-    function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
-         */
-        allEnemies.forEach(function(enemy) {
+    // 渲染操作元素函数
+    function renderUntities() {
+        // 玩家渲染
+        player.render();
+        // 敌人渲染
+        allEnemies.forEach(function (enemy) {
             enemy.render();
         });
-
-        player.render();
     }
 
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
-     */
-    function reset() {
-        // noop
-    }
-
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
-     */
+    // 素材加载，用到了上期的 Resources 对象
     Resources.load([
         'images/stone-block.png',
         'images/water-block.png',
@@ -174,11 +106,8 @@ var Engine = (function(global) {
         'images/enemy-bug.png',
         'images/char-boy.png'
     ]);
+
+    // 加载完触发回调函数
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developers can use it more easily
-     * from within their app.js files.
-     */
-    global.ctx = ctx;
-})(this);
+})(this, 505, 606);
--- js/resources.js
+++ js/resources.js
@@ -1,111 +1,77 @@
-/* Resources.js
- * This is simply an image loading utility. It eases the process of loading
- * image files so that they can be used within your game. It also includes
- * a simple "caching" layer so it will reuse cached images if you attempt
- * to load the same image multiple times.
- */
-(function() {
-    var resourceCache = {};
-    var loading = [];
-    var readyCallbacks = [];
+(function () {
 
-    /* This is the publicly accessible image loading function. It accepts
-     * an array of strings pointing to image files or a string for a single
-     * image. It will then call our private image loading function accordingly.
-     */
-    function load(urlOrArr) {
-        if(urlOrArr instanceof Array) {
-            /* If the developer passed in an array of images
-             * loop through each value and call our image
-             * loader on that image file
-             */
-            urlOrArr.forEach(function(url) {
-                _load(url);
+    // 存缓存图片
+    var imageCache = [];
+    // 图片加载完后的回调函数
+    var callBacks = [];
+
+    // 加载函数
+    function load(imageUrl) {
+        // 判断是单个照片还是多个照片
+        if (imageUrl instanceof Array) {
+            imageUrl.forEach(function (url) {
+                loadUrl(url);
             });
-        } else {
-            /* The developer did not pass an array to this function,
-             * assume the value is a string and call our image loader
-             * directly.
-             */
-            _load(urlOrArr);
+        }
+        else {
+            loadUrl(url);
         }
     }
 
-    /* This is our private image loader function, it is
-     * called by the public image loader function.
-     */
-    function _load(url) {
-        if(resourceCache[url]) {
-            /* If this URL has been previously loaded it will exist within
-             * our resourceCache array. Just return that image rather
-             * re-loading the image.
-             */
-            return resourceCache[url];
-        } else {
-            /* This URL has not been previously loaded and is not present
-             * within our cache; we'll need to load this image.
-             */
-            var img = new Image();
-            img.onload = function() {
-                /* Once our image has properly loaded, add it to our cache
-                 * so that we can simply return this image if the developer
-                 * attempts to load this file in the future.
-                 */
-                resourceCache[url] = img;
+    // 加载处理函数
+    function loadUrl(url) {
+        // 有缓存，直接返回
+        if (imageCache[url]) {
+            return imageCache[url];
+        }
+        // 没有缓存，创建缓存
+        else {
+            var image = new Image();
+            // 加载完成后，加入缓存数组
+            image.onload = function () {
+                imageCache[url] = image;
 
-                /* Once the image is actually loaded and properly cached,
-                 * call all of the onReady() callbacks we have defined.
-                 */
-                if(isReady()) {
-                    readyCallbacks.forEach(function(func) { func(); });
+                // 所有图片加载完后，执行函数
+                if (isReady()) {
+                    callBacks.forEach(function (func) {
+                        func();
+                    });
                 }
-            };
-
-            /* Set the initial cache value to false, this will change when
-             * the image's onload event handler is called. Finally, point
-             * the image's src attribute to the passed in URL.
-             */
-            resourceCache[url] = false;
-            img.src = url;
+            }
+            // 监听事件放在触发事件之前
+            imageCache[url] = false;
+            image.src = url;
         }
     }
 
-    /* This is used by developers to grab references to images they know
-     * have been previously loaded. If an image is cached, this functions
-     * the same as calling load() on that URL.
-     */
+    // 获取某个缓存图片
     function get(url) {
-        return resourceCache[url];
+        return imageCache[url];
     }
 
-    /* This function determines if all of the images that have been requested
-     * for loading have in fact been properly loaded.
-     */
+    // 判断图片是否完全加载完，执行回调函数
     function isReady() {
         var ready = true;
-        for(var k in resourceCache) {
-            if(resourceCache.hasOwnProperty(k) &&
-               !resourceCache[k]) {
+        for (var img in imageCache) {
+            if (!imageCache[img]) {
                 ready = false;
+                break;
             }
         }
+
         return ready;
     }
 
-    /* This function will add a function to the callback stack that is called
-     * when all requested images are properly loaded.
-     */
+    // 加载执行函数
     function onReady(func) {
-        readyCallbacks.push(func);
+        callBacks.push(func);
     }
 
-    /* This object defines the publicly accessible functions available to
-     * developers by creating a global Resources object.
-     */
     window.Resources = {
         load: load,
         get: get,
         onReady: onReady,
         isReady: isReady
     };
+
 })();
