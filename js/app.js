46c36c40b2aae6d3e675a616d2617f533206c344
Joey Qinag <joeyqiang@gmail.com>
2017-07-17 22:13:17 +0800

    done

--- js/app.js
+++ js/app.js
@@ -1,85 +1,295 @@
-// Enemies our player must avoid
-var Enemy = function (x, y, width, height, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Player = function (x, y, w, h, lives) {
     this.x = x;
     this.y = y;
-    this.originY = y;
-    this.width = width;
-    this.height = height;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.originalX = x;
+    this.originalY = y;
+    this.originalLives = lives;
+    this.width = w;
+    this.height = h;
+    this.lives = lives;
+    this.score = 0;
+    this.success = false;
+    this.gameOver = false;
+    this.image = 'images/player.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype = {
-    // Move enemy on the screen
-    update: function (dt) {
-        this.x += this.speed * dt;
-        if (this.x >= 470) {
-            this.reset();
+Player.prototype = {
+    checkCollision: function (player, objectList) {
+        var type = isCollided(player, objectList);
+        if (type !== 'undefined') {
+            if (type === 'enemy') {
+                this.lives--;
+                player.moveBack();
+                if (this.lives === 0) {
+                    // 游戏失败结束
+                    this.gameOver = true;
+                }
+            }
+            else if (type === 'collection') {
+                if (this.score === 5) {
+                    // 游戏获胜结束
+                    this.success = true;
+                    this.gameOver = true;
+                }
+            }
+            else if (type === 'barrier') {
+                // this.collided = true;
+            }
         }
     },
-    reset: function () {
-        this.x = Math.random() * -300;
+    moveBack: function () {
+        this.x = this.originalX;
         this.y = this.originalY;
     },
-// Draw enemy on the screen
+    reset: function () {
+        this.x = this.originalX;
+        this.y = this.originalY;
+        this.lives = this.originalLives;
+        this.score = 0;
+        this.success = false;
+        this.gameOver = false;
+    },
+    update: function () {
+        this.checkCollision(player, allNonBarriers);
+    },
     render: function () {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.image), this.x, this.y);
+    },
+    handleInput: function (keyCode) {
+        if (player.gameOver === true) {
+            return;
+        }
+        // 像左移动
+        if (keyCode === 'left' && this.x > 114) {
+            var objB = clonePosition(player, keyCode);
+            if (isCollided(objB, allBarriers) !== 'barrier') {
+                this.x -= 115;
+            }
+        }
+        // 像右移动
+        else if (keyCode === 'right' && this.x < 577) {
+            var objB = clonePosition(player, keyCode);
+            if (isCollided(objB, allBarriers) !== 'barrier') {
+                this.x += 115;
+            }
+        }
+        // 像上移动
+        else if (keyCode === 'up' && this.y > 0) {
+            var objB = clonePosition(player, keyCode);
+            if (isCollided(objB, allBarriers) !== 'barrier') {
+                this.y -= 115;
+            }
+        }
+        // 像下移动
+        else if (keyCode === 'down' && this.y < 575) {
+            var objB = clonePosition(player, keyCode);
+            if (isCollided(objB, allBarriers) !== 'barrier') {
+                this.y += 115;
+            }
+        }
     }
 };
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function (x, y, width, height, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function (x, y, w, h, speed) {
+    this.x = x - 114 * Math.random();
+    this.y = y;
+    this.originalX = this.x;
+    this.originalY = this.y;
+    this.width = w;
+    this.height = h;
+    this.image = 'images/enemy.png';
+    this.type = 'enemy';
+    this.speed = speed + speed * Math.random();
+};
+
+Enemy.prototype = {
+    reset: function () {
+        this.x = this.originalX;
+        this.y = this.originalY;
+    },
+    update: function (dt) {
+        if (this.x > 798) {
+            this.x = 798 + 114 * Math.random();
+            this.speed = this.speed + (600 - this.speed) * Math.random();
+            this.speed = -this.speed;
+        }
+        else if (this.x < -114) {
+            this.x = -114 - 114 * Math.random();
+            this.speed = -this.speed;
+            this.speed = this.speed + (600 - this.speed) * Math.random();
+        }
+        this.x += this.speed * dt;
+    },
+    render: function () {
+        ctx.drawImage(Resources.get(this.image), this.x, this.y);
+    }
+};
+
+var Barrier = function (x, y, w, h) {
     this.x = x;
     this.y = y;
-    this.originY = y;
-    this.width = width;
-    this.height = height;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.width = w;
+    this.height = h;
+    this.image = 'images/barrier.png';
+    this.type = 'barrier';
 };
-Player.prototype = {
+
+Barrier.prototype = {
+    reset: function () {
+
+    },
     update: function () {
 
     },
     render: function () {
+        ctx.drawImage(Resources.get(this.image), this.x, this.y);
+    }
+};
+
+var Collection = function (x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.image = 'images/basketball.png';
+    this.type = 'collection';
+};
+
+Collection.prototype = {
+    reset: function () {
+        allNonBarriers.length = 5;
+        allNonBarriers.push(basketballOne);
+        allNonBarriers.push(basketballTwo);
+        allNonBarriers.push(basketballThree);
+        allNonBarriers.push(basketballFour);
+        allNonBarriers.push(basketballFive);
+    },
+    update: function () {
 
     },
-    handleInput: function (keyCode) {
-
+    render: function () {
+        ctx.drawImage(Resources.get(this.image), this.x, this.y);
+    },
+    remove: function () {
+        var index;
+        for (var i = 0; i < allNonBarriers.length; i++) {
+            if (this == allNonBarriers[i]) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            allNonBarriers.splice(index, 1);
+            player.score++;
+        }
     }
+};
+
+function isCollided(objA, objectsList) {
+    //返回碰撞物的类别
+    var type = undefined;
+    objA.t = objA.y;
+    objA.b = objA.t + objA.height;
+    objA.l = objA.x;
+    objA.r = objA.l + objA.width;
+    // 对象的上下左右，循环判断是否有冲突
+    objectsList.forEach(function (object) {
+        object.t = object.y;
+        object.b = object.t + object.height;
+        object.l = object.x;
+        object.r = object.l + object.width;
+
+        if (objA.t > object.b || objA.b < object.t || objA.l > object.r || objA.r < object.l) {
+        }
+        else {
+            type = object.type;
+            if (type == 'collection') {
+                object.remove();
+            }
+        }
+    })
+    return type;
+};
+
+function clonePosition(objA, direction) {
+    var objB = {};
+    objB.x = objA.x;
+    objB.y = objA.y;
+    objB.width = objA.width;
+    objB.height = objA.height;
+
+    if (direction == 'left') {
+        objB.x -= 15;
+    }
+    else if (direction == 'right') {
+        objB.x += 15;
+    }
+    else if (direction == 'up') {
+        objB.y -= 15;
+    }
+    else if (direction == 'down') {
+        objB.y += 15;
+    }
+    return objB;
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [];
-var enemyOne = new Enemy(15, 139, 80, 62, 100);
-allEnemies.push(enemyOne);
-var player = new Player(218.5, 467);
+
+//初始化对象
+//初始化玩家
+var player = new Player(345, 575, 114, 114, 3);
+
+//初始化敌人
+var allNonBarriers = [];
+var enemyOne = new Enemy(-114, 0, 114, 114, 300);
+var enemyTwo = new Enemy(-114, 115, 114, 114, 300);
+var enemyThree = new Enemy(-114, 230, 114, 114, 300);
+var enemyFour = new Enemy(-114, 345, 114, 114, 300);
+var enemyFive = new Enemy(-114, 460, 114, 114, 300);
+allNonBarriers.push(enemyOne);
+allNonBarriers.push(enemyTwo);
+allNonBarriers.push(enemyThree);
+allNonBarriers.push(enemyFour);
+allNonBarriers.push(enemyFive);
+
+//初始化收集物
+var basketballOne = new Collection(0, 0, 114, 114);
+var basketballTwo = new Collection(690, 0, 114, 114);
+var basketballThree = new Collection(460, 115, 114, 114);
+var basketballFour = new Collection(115, 230, 114, 114);
+var basketballFive = new Collection(690, 345, 114, 114);
+allNonBarriers.push(basketballOne);
+allNonBarriers.push(basketballTwo);
+allNonBarriers.push(basketballThree);
+allNonBarriers.push(basketballFour);
+allNonBarriers.push(basketballFive);
+
+//初识化障碍物
+var allBarriers = [];
+var barrierOne = new Barrier(115, 115, 114, 114);
+var barrierTwo = new Barrier(575, 115, 114, 114);
+var barrierThree = new Barrier(230, 230, 114, 114);
+var barrierFour = new Barrier(690, 230, 114, 114);
+var barrierFive = new Barrier(575, 345, 114, 114);
+
+allBarriers.push(barrierOne);
+allBarriers.push(barrierTwo);
+allBarriers.push(barrierThree);
+allBarriers.push(barrierFour);
+allBarriers.push(barrierFive);
+
+//所有物体数组
+var objectList = allBarriers.concat(allNonBarriers);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function (e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
 
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+
+
+
+
+
+
+
+
+
+
--- js/engine.js
+++ js/engine.js
@@ -1,113 +1,184 @@
-// 传入全局变量 this->global
-var Engine = (function (global, width, height) {
-    // 定义绘制动画需要的参数
-    var doc = global.document,
-        win = global.window,
+//         // 渲染操作元素
+//         renderUntities();
+
+var Engine = (function (global) {
+
+    //建立canvas对象
+    var win = window,
+        doc = win.document,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
-        // 定义画布为全局变量，便于其他 JS 文件调用
-        global.ctx = ctx;
-
+        lastTime,
+        // 图片地址只是一个匹配符
+        rowImage = 'images/bg_tile.png';
+    // 定义画布为全局变量，便于其他 JS 文件调用
+    global.ctx = ctx;
     // 定义画布大小
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = 798;
+    canvas.height = 684;
     doc.body.appendChild(canvas);
 
-    // 初始化函数
-    function init() {
-        // 还原
-        reset();
-        // 时间戳，记录的是千分之一秒值
-        lastTime = Date.now();
-        // 主函数
-        main();
+    //游戏对象属性
+    var Game = function () {
+        this.pause = false;
+        this.now = Date.now();
+        this.nextTime = undefined;
+        this.rows = 6;
+        this.cols = 7;
     }
 
-
-    // 主函数，画布不断刷新，同时更新画布内容
-    function main() {
-        var now = Date.now();
-        dt = (now - lastTime) / 1000
-        // 画面内容更新，显示器未更新
-        update(dt);
-        // 画面内容渲染，显示器未更新
-        render();
-        // 时间戳
-        lastTime = now;
-        // Javascript 每 1/60 FPS 刷新一次，类似 setTimeout(main, 1000/60)
-        win.requestAnimationFrame(main);
-    }
-
-    // 还原函数
-    function reset() {
-        // 游戏重新执行
-        // restart();
-    }
-
-    // 更新函数
-    function update(dt) {
-        updateUntities(dt);
-        // 检验是否发生碰撞
-        // checkCollisions();
-    }
-
-    // 更新细节
-    function updateUntities(dt) {
-        // 玩家更新，一个玩家
-        player.update();
-        // 敌人更新，多个敌人
-        allEnemies.forEach(function (enemy) {
-            enemy.update(dt);
-        });
-    }
-
-    // 渲染函数
-    function render() {
-        var rowImages = [
-                'images/water-block.png',   // 第一行图片
-                'images/stone-block.png',   // 第二行图片
-                'images/stone-block.png',   // 第三行图片
-                'images/stone-block.png',   // 第四行图片
-                'images/grass-block.png',   // 第五行图片
-                'images/grass-block.png'    // 第六行图片
-            ],
-            rows = 6,
-            cols = 5,
-            row,
-            col;
-
-        // 定位背景图片位置，图片大小 101 * 83
-        for (row = 0; row < rows; row++) {
-            for (col = 0; col < cols; col++) {
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 82);
+    //游戏方法
+    Game.prototype = {
+        main: function () {
+            var _this = this;
+            this.nextTime = Date.now();
+            var dt = (this.nextTime - this.now) / 1000;
+            this.now = this.nextTime;
+            this.update(dt);
+            this.init();
+            this.render();
+            this.over();
+            window.requestAnimationFrame(function(){
+                _this.main();
+            });
+        },
+        start: function () {
+            this.main();
+        },
+        init: function () {
+            this.drawCanvas();
+        },
+        update: function (dt) {
+            allBarriers.forEach(function(barrier){
+                barrier.update();
+            });
+            allNonBarriers.forEach(function(enemy){
+                enemy.update(dt);
+            });
+            player.update();
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        },
+        render: function () {
+            allBarriers.forEach(function(barrier){
+                barrier.render();
+            });
+            allNonBarriers.forEach(function(enemy){
+                enemy.render();
+            });
+            player.render();
+        },
+        pause: function () {
+            player.update(0);
+            allNonBarriers.forEach(function(enemy){
+                enemy.update(0);
+            });
+        },
+        over: function () {
+            if(player.gameOver === true){
+                this.showOver();
+            }
+        },
+        showOver: function () {
+            var text;
+            if (player.success === false) {
+                text = 'GAME OVER';
+            }
+            else {
+                text = 'YOU WIN';
+            }
+            ctx.font = '23pt Arial';
+            ctx.globalAlpha = 0.65;
+            ctx.fillStyle = 'black';
+            ctx.fillRect(228, 200, 350, 200);
+            ctx.globalAlpha = 1;
+            ctx.fillStyle = 'yellow';
+            // text要居中
+            ctx.textAlign = 'center';
+            ctx.fillText(text, 400, 285);
+            ctx.font = '18pt Arial';
+            ctx.fillStyle = 'white';
+            ctx.fillText('Press Enter to Play Again', 400, 350);
+        },
+        reset: function () {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            player.reset();
+            allNonBarriers.forEach(function(enemy){
+                enemy.reset();
+            });
+            // allBarriers.forEach(function(barrier){
+            //     barrier.reset();
+            // });
+        },
+        drawCanvas: function () {
+            for(var row = 0; row < this.rows; row++){
+                for(var col = 0; col < this.cols; col++){
+                    ctx.drawImage(Resources.get(rowImage), col * 115, row * 115);
+                }
+            }
+            // 画界面的基础元素
+            // 字体
+            ctx.font = '20pt Arial';
+            // 边框颜色
+            ctx.strokeStyle = 'black';
+            // 边框粗细
+            ctx.lineWidth = 3;
+            // 填充色
+            ctx.fillStyle = 'yellow';
+            // 填充内容
+            ctx.textAlign = 'start';
+            ctx.strokeText('收集篮球数: ' + player.score + ' / 5', 565, 630);
+            ctx.fillText('收集篮球数: ' + player.score + ' / 5', 565, 630);
+            ctx.drawImage(Resources.get('images/heart.png'), 20, 636);
+            ctx.strokeStyle = 'white';
+            ctx.lineWidth = 4;
+            ctx.strokeText('x', 58, 658);
+            ctx.strokeText(player.lives, 86, 660);
+            ctx.fillStyle = 'black';
+            ctx.fillText('x', 58, 658);
+            ctx.fillText(player.lives, 86, 660);
+            ctx.font = '12.5pt Arial';
+            ctx.fillStyle = 'white';
+            ctx.globalAlpha = 0.7;
+            // ctx.fillText('Press space to pause or resume', 290, 572);
+            ctx.fillText('上下左右控制角色移动', 600, 660);
+            ctx.globalAlpha = 1;
+        },
+        handleInput: function (keyCode){
+            if(keyCode === 'enter'){
+                game.reset();
+                game.start();
             }
         }
+    };
 
-        // 渲染操作元素
-        renderUntities();
-    }
+    // 初始化游戏，游戏开始
+    var game = new Game();
 
-    // 渲染操作元素函数
-    function renderUntities() {
-        // 玩家渲染
-        player.render();
-        // 敌人渲染
-        allEnemies.forEach(function (enemy) {
-            enemy.render();
-        });
-    }
-
-    // 素材加载，用到了上期的 Resources 对象
+    // 素材加载，图片列表
     Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/bg_tile.png',
+        'images/enemy.png',
+        'images/player.png',
+        'images/basketball.png',
+        'images/barrier.png',
+        'images/heart.png'
     ]);
 
     // 加载完触发回调函数
-    Resources.onReady(init);
+    Resources.onReady(function(){
+        game.start();
+    });
 
-})(this, 505, 606);
+    //事件监听
+    document.addEventListener('keydown', function (e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down',
+            13: 'enter'
+        };
+        player.handleInput(allowedKeys[e.keyCode]);
+        game.handleInput(allowedKeys[e.keyCode]);
+    });
+})(this);
